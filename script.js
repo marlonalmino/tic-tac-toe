1c4365b4086e4b7a9d0bb6e2eca8baecb71880bc
Marlon Almino <alminomarlon@gmail.com>
2022-03-07 16:52:59 -0300

    functions to be implemented

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ const drawMessage = () => `Game ended in a draw!`
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`
 
 statusDisplay.innerHTML = currentPlayerTurn()
+
+function handleCellPlayed() {}
+
+function handlePlayerChange() {}
+
+function handleResultValidation() {}
+
+function handleCellClick() {}
+
+function handleRestartGame() {}
+
+document
+  .querySelectorAll('.cell')
+  .forEach((cell) => cell.addEventListener('click', handleCellClick))
+
+document
+  .querySelector('.game--restart')
+  .addEventListener('click', handleRestartGame)
