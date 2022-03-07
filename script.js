57cff961ba4c30252de3d37c5a02c41267ec64e9
Marlon Almino <alminomarlon@gmail.com>
2022-03-07 17:07:19 -0300

    implemented function - handleCellClick

--- script.js
+++ script.js
@@ -18,7 +18,18 @@ function handlePlayerChange() {}
 
 function handleResultValidation() {}
 
-function handleCellClick() {}
+function handleCellClick(clickedCellEvent) {
+  const clickedCell = clickedCellEvent.target
+
+  const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'))
+
+  if (gameState[clickedCellIndex] !== '' || !gameActive) {
+    return
+  }
+
+  handleCellPlayed(clickedCell, clickedCellIndex)
+  handleResultValidation()
+}
 
 function handleRestartGame() {}
 
