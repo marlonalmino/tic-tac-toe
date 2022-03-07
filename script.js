e3d3482a62b5aeee48fb2ff734de88968e2fb710
Marlon Almino <alminomarlon@gmail.com>
2022-03-07 17:21:48 -0300

    implementing handleResultValidation and roundWon

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ let currentPlayer = 'X'
 
 let gameState = ['', '', '', '', '', '', '', '', '']
 
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
 const winningMessage = () => `Player ${currentPlayer} has won!`
 const drawMessage = () => `Game ended in a draw!`
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`
@@ -19,7 +30,31 @@ function handleCellPlayed(clickedCell, clickedCellIndex) {
 
 function handlePlayerChange() {}
 
-function handleResultValidation() {}
+function handleResultValidation() {
+  let roundWon = false
+
+  for (let i = 0; i <= 7; i++) {
+    const winContidion = winningConditions[i]
+    let a = gameState[winContidion[0]]
+    let b = gameState[winContidion[1]]
+    let c = gameState[winContidion[2]]
+
+    if (a === '' || b === '' || c === '') {
+      continue
+    }
+
+    if (a === b && b === c) {
+      roundWon = true
+      break
+    }
+  }
+
+  if (roundWon) {
+    statusDisplay.innerHTML = winningMessage()
+    gameActive = false
+    return
+  }
+}
 
 function handleCellClick(clickedCellEvent) {
   const clickedCell = clickedCellEvent.target
