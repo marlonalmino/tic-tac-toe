014c6b4f8a692890d2385c535829a6a8e6937ff8
Marlon Almino <alminomarlon@gmail.com>
2022-03-07 16:19:09 -0300

    first implement in html, css and js files

---
+++ script.js
@@ -0,0 +1,13 @@
+const statusDisplay = document.querySelector('.game--status')
+
+let gameActive = true
+
+let currentPlayer = 'X'
+
+let gameState = ['', '', '', '', '', '', '', '', '']
+
+const winningMessage = () => `Player ${currentPlayer} has won!`
+const drawMessage = () => `Game ended in a draw!`
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`
+
+statusDisplay.innerHTML = currentPlayerTurn()
